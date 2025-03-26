77e1cde3f0301e7b97a6fc26c2c8037c8c446dc9
cevastianchoi <seb.dev.it@gmail.com>
2025-03-26 11:58:11 +0900

    Spring React login main no-jwt init

---
+++ src/main/java/com/spring/daon/service/LoginServiceImpl.java
@@ -0,0 +1,20 @@
+package com.spring.daon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.daon.dao.LoginMapper;
+import com.spring.daon.dto.LoginDTO;
+
+@Service 
+public class LoginServiceImpl implements LoginService {
+
+	@Autowired
+	private LoginMapper loginMapper; 
+	
+    @Override
+    public int checkIdPwd(LoginDTO dto) {
+        
+            return loginMapper.checkIdPwd(dto);
+    }
+}
