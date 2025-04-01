b821d3fe4a54cc70a58fc1bb93ea83f2332d8d23
psy1-dev <psy1.dev@gmail.com>
2025-04-01 16:46:00 +0900

    web crwaling python file

--- front-react/src/messenger/pages/Messenger.js
+++ front-react/src/messenger/pages/Messenger.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {
+  Button,
+  ButtonToolbar,
   Container,
   Content,
+  IconButton,
 } from 'rsuite';
 
 import Leftbar from '../../common/pages/Leftbar';
@@ -11,16 +14,24 @@ const Messenger = () => {
     <div>
       <Container style={{ minHeight: '100vh', width: '100%' }}>
         <Leftbar />
-        <Container>
+        <div style={{display:'flex', maxHeight: '35px'}}>
+          <input placeholder='이름/이메일검색'>
+
+          </input>
+          <ButtonToolbar >
+            <Button style={{backgroundColor:'#BEB1E3'}}>검색</Button>
+          </ButtonToolbar>
+        </div>
+        {/* <Container>
           <Content>Messenger
             <Content>
-              <img src="/image/chat_list.jpg" alt="Daon" style={{ width: '300px', height: 'auto' }} />
+              
               <img src="/image/Contact.jpg" alt="Daon" style={{ width: '300px', height: 'auto' }} />
-              <img src="/image/chat_list.jpg" alt="Daon" style={{ width: '300px', height: 'auto' }} />
             </Content>
           </Content>
-        </Container>
+        </Container> */}
       </Container>
+      <img src="/image/chat_list.jpg" alt="Daon" style={{ width: '300px', height: 'auto' }} />
     </div>
   );
 };
